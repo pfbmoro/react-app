8a50e79fce25db977c03be8ea73bdbdf72adf1ac
Paulo Moro <pfmoro@icloud.com>
2024-04-12 10:54:30 +0100

    First commit of my-second-app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './second-app-logo.png';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is my second App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
